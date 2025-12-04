40498f128c0b6a42acbf5c978a4a9539545344e9
Anoop George <anoopgeorge418@gmail.com>
2025-12-05 00:29:51 +0530

    Admin Initial Project Setup

---
+++ admin/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Sora } from "next/font/google";
+import "./globals.css";
+
+const sora = Sora({
+  variable: "--font-sora",
+  subsets: ["latin"],
+  weight: ['300', 400', '700'],
+});
+
+export const metadata: Metadata = {
+  title: "BingeParty - Admin Panel",
+  description: "A fully strucutred admin panel for BingeParty mobile application.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${sora.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
