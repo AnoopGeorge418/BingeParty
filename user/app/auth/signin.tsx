7e0c62f4b6aeb53a49569b143fa948decfbb69cd
Anoop George <anoopgeorge418@gmail.com>
2025-12-05 00:09:20 +0530

    Ui design created for Intro, Signup and Signin Pages

---
+++ user/app/auth/signin.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Signin = () => {
+    return (
+        <View className="bg-background flex-1">
+            <Text className="text-white">This is signin page</Text>
+        </View>
+    )
+}
+
+export default Signin
